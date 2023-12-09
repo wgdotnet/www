651fed91adff56cd0041569b86c4508651405935
Maciek Misztal <mmisztal1980@gmail.com>
2023-12-09 11:48:58 +0100

    chore: footer translations

--- src/components/Footer.i18n.ts
+++ src/components/Footer.i18n.ts
@@ -2,9 +2,19 @@ export const i18n: { [k1: string]: { [k2: string]: any } } = {
     en: {
       wgdotnet: 'The Warsaw .NET Usergroup',
       supportedBy: 'Supported by',
+      logoCaption: 'Przyjazna przestrzeń dla pasjonatów .NET',
+      aboutUS: 'O nas',
+      contactUs: 'Kontakt',
+      legal: 'Informacje prawne',
+      privacy: 'Polityka prywatności'
     },
     pl: {
       wgdotnet: 'Warszawska Grupa .NET',
       supportedBy: 'Wspierają nas',
+      logoCaption: 'A friendly space for .NET enthusiasts',
+      aboutUS: 'About us',
+      contactUs: 'Contact us',
+      legal: 'Legal',
+      privacy: 'Privacy policy'
     },
   } as const;
