3428721afb975f1722d8f3f0edb1d8778df50217
Maciek Misztal <mmisztal1980@gmail.com>
2023-12-09 11:55:41 +0100

    fix: translations keys

--- src/components/Footer.i18n.ts
+++ src/components/Footer.i18n.ts
@@ -3,18 +3,18 @@ export const i18n: { [k1: string]: { [k2: string]: any } } = {
       wgdotnet: 'The Warsaw .NET Usergroup',
       supportedBy: 'Supported by',
       logoCaption: 'A friendly space for .NET enthusiasts',
-      aboutUS: 'O nas',
-      contactUs: 'Kontakt',
-      legal: 'Informacje prawne',
-      privacy: 'Polityka prywatności'
+      aboutUS: 'About us',
+      contactUs: 'Contact us',
+      legal: 'Legal',
+      privacy: 'Privacy policy'
     },
     pl: {
       wgdotnet: 'Warszawska Grupa .NET',
       supportedBy: 'Wspierają nas',
       logoCaption: 'Przyjazna przestrzeń dla pasjonatów .NET',
-      aboutUS: 'About us',
-      contactUs: 'Contact us',
-      legal: 'Legal',
-      privacy: 'Privacy policy'
+      aboutUS: 'O nas',
+      contactUs: 'Kontakt',
+      legal: 'Informacje prawne',
+      privacy: 'Polityka prywatności'
     },
   } as const;
