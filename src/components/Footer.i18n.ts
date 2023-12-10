6fdd3d584212427fbc31a998f9677d376493b3c5
Maciek Misztal <mmisztal1980@gmail.com>
2023-12-10 15:07:36 +0100

    fix: key

--- src/components/Footer.i18n.ts
+++ src/components/Footer.i18n.ts
@@ -3,7 +3,7 @@ export const i18n: { [k1: string]: { [k2: string]: any } } = {
       wgdotnet: 'The Warsaw .NET Usergroup',
       supportedBy: 'Supported by',
       logoCaption: 'A friendly space for .NET enthusiasts',
-      aboutUS: 'About us',
+      aboutUs: 'About us',
       contactUs: 'Contact us',
       legal: 'Legal',
       privacy: 'Privacy policy'
@@ -12,7 +12,7 @@ export const i18n: { [k1: string]: { [k2: string]: any } } = {
       wgdotnet: 'Warszawska Grupa .NET',
       supportedBy: 'Wspierają nas',
       logoCaption: 'Przyjazna przestrzeń dla pasjonatów .NET',
-      aboutUS: 'O nas',
+      aboutUs: 'O nas',
       contactUs: 'Kontakt',
       legal: 'Informacje prawne',
       privacy: 'Polityka prywatności'
