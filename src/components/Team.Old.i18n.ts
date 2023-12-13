6568aa8ade95b159b1c484bebd44803a80ee4488
Maciek Misztal <mmisztal1980@gmail.com>
2023-12-13 18:42:41 +0100

    feat: pawel potasinski

--- src/components/Team.Old.i18n.ts
+++ src/components/Team.Old.i18n.ts
@@ -1,10 +1,10 @@
 export const i18n: { [k1: string]: { [k2: string]: string } } = {
     en: {
-      heading: 'Meet the previous wgdotnet leaders',
+      heading: 'Meet the previous wgdotnet leaders & coorganizers',
       subheading: 'For community, by community'
     },
     pl: {
-      heading: 'Poznaj poprzednich liderów wgdotnet',
+      heading: 'Poznaj poprzednich liderów i koorganizatorów wgdotnet',
       subheading: 'Od społeczności, dla społeczności'
     },
   } as const;
