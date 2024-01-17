80143f0456853685122c1275b99b787202f5f695
Maciek Misztal <mmisztal1980@gmail.com>
2024-01-17 22:25:25 +0100

    chore: update mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -35,124 +35,115 @@ export default defineConfig({
           }
       },
       languages: {
-        en: {
+        'en': {
+            consent_modal: {
+                title: 'We use cookies!',
+                description: 'Hi, this website uses essential cookies to ensure its proper operation. <button type="button" data-cc="c-settings" class="cc-link">Settings</button>',
+                primary_btn: {
+                    text: 'Accept all',
+                    role: 'accept_all'
+                },
+                secondary_btn: {
+                    text: 'Reject all',
+                    role: 'accept_necessary'
+                }
+            },
+            settings_modal: {
+                title: logo,
+                save_settings_btn: 'Save settings',
+                accept_all_btn: 'Accept all',
+                reject_all_btn: 'Reject all',
+                close_btn_label: 'Close',
+                cookie_table_headers: [
+                    {col1: 'Name'},
+                    {col2: 'Domain'},
+                    {col3: 'Expiration'},
+                    {col4: 'Description'}
+                ],
+                blocks: [
+                    {
+                        title: 'Cookie usage',
+                        description: 'We use cookies to ensure the basic functionalities of the website and to enhance your online experience.'
+                    }, {
+                        title: 'Strictly necessary cookies',
+                        description: 'These cookies are essential for the proper functioning of the website. Without these cookies, the website would not work properly',
+                        toggle: {
+                            value: 'necessary',
+                            enabled: true,
+                            readonly: true
+                        },
+                        cookie_table: [
+                          {
+                              col1: '^_wgdotnet',
+                              col2: 'wgdotnet.pl',
+                              col3: '90(s)',
+                              col4: 'technical cookies',
+                              is_regex: true
+                          }
+                      ]
+                    },
+                    {
+                        title: 'More information',
+                        description: 'For any queries in relation to our policies on cookies, privacy and your choices, please: <br/><ul><li>- Review the <a href="/privacy" class="cc-link">privacy policy</a></li><li>- Review the <a href="/cookies" class="cc-link">cookies policy</a></li><li>- <a class="cc-link" href="mailto:leaders@wgdotnet.pl">Contact us</a></li></ul>',
+                    }
+                ]
+            }
+        },
+        'pl': {
           consent_modal: {
-            title: "Hello traveller, it's cookie time!",
-            description:
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.",
-            acceptAllBtn: "Accept all",
-            acceptNecessaryBtn: "Reject all",
-            showPreferencesBtn: "Manage preferences",
-            footer:
-              '<a href="/privacy">Privacy Policy</a>\n<a href="#link">Terms and conditions</a>',
+              title: 'Używamy plików cookie(s)!',
+              description: 'Witaj, ta strona używa plików cookie(s), by zapewnić jej poprawne funkcjonowanie. <button type="button" data-cc="c-settings" class="cc-link">Ustawienia</button>',
+              primary_btn: {
+                  text: 'Zaakceptuj wszystkie',
+                  role: 'accept_all'
+              },
+              secondary_btn: {
+                  text: 'Zaakceptuj wymagane',
+                  role: 'accept_necessary'
+              }
           },
           settings_modal: {
-            title: "Consent Preferences Center",
-            acceptAllBtn: "Accept all",
-            acceptNecessaryBtn: "Reject all",
-            savePreferencesBtn: "Save preferences",
-            closeIconLabel: "Close modal",
-            serviceCounterLabel: "Service|Services",
-            cookie_table_headers: [
-              {col1: 'Name'},
-              {col2: 'Domain'},
-              {col3: 'Expiration'},
-              {col4: 'Description'}
-          ],
-          blocks: [
-            {
-                title: 'Cookie usage 📢',
-                description: 'I use cookies to ensure the basic functionalities of the website and to enhance your online experience. You can choose for each category to opt-in/out whenever you want. For more details relative to cookies and other sensitive data, please read the full <a href="#" class="cc-link">privacy policy</a>.'
-            }, {
-                title: 'Strictly necessary cookies',
-                description: 'These cookies are essential for the proper functioning of my website. Without these cookies, the website would not work properly',
-                toggle: {
-                    value: 'necessary',
-                    enabled: true,
-                    readonly: true          // cookie categories with readonly=true are all treated as "necessary cookies"
-                }
-            }, {
-                title: 'Performance and Analytics cookies',
-                description: 'These cookies allow the website to remember the choices you have made in the past',
-                toggle: {
-                    value: 'analytics',     // your cookie category
-                    enabled: false,
-                    readonly: false
-                },
-                cookie_table: [             // list of all expected cookies
-                    {
-                        col1: '^_ga',       // match all cookies starting with "_ga"
-                        col2: 'google.com',
-                        col3: '2 years',
-                        col4: 'description ...',
-                        is_regex: true
-                    },
-                    {
-                        col1: '_gid',
-                        col2: 'google.com',
-                        col3: '1 day',
-                        col4: 'description ...',
-                    },
-                    {
-                        col1: '_fbp',
-                        col2: '.ddev.site',
-                        col3: '1 day',
-                        col4: 'description ...',
-                    },
-                       {
-                        col1: 'ln_or',
-                        col2: '.ddev.site',
-                        col3: '1 day',
-                        col4: 'description ...',
-                    },
-                       {
-                        col1: '_gat',
-                        col2: '.ddev.site',
-                        col3: '1 day',
-                        col4: 'description ...',
-                    },
-                ]
-            }, {
-                title: 'Advertisement and Targeting cookies',
-                description: 'These cookies collect information about how you use the website, which pages you visited and which links you clicked on. All of the data is anonymized and cannot be used to identify you',
-                toggle: {
-                    value: 'targeting',
-                    enabled: false,
-                    readonly: false
-                }
-            }, {
-                title: 'More information',
-                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="#yourcontactpage">contact us</a>.',
-            }
-        ],
-            sections: [
-              {
-                title: "Cookie Usage",
-                description:
-                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-              },
-              {
-                title:
-                  'Strictly Necessary Cookies <span class="pm__badge">Always Enabled</span>',
-                description:
-                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-                linkedCategory: "necessary",
-              },
-              {
-                title: "Analytics Cookies",
-                description:
-                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-                linkedCategory: "analytics",
-              },
-              {
-                title: "More information",
-                description:
-                  'For any query in relation to my policy on cookies and your choices, please <a class="cc__link" href="#yourdomain.com">contact me</a>.',
-              },
-            ],
-          },
-        },
+              title: logo,
+              save_settings_btn: 'Zapisz ustawienia',
+              accept_all_btn: 'Zaakceptuj wszystkie',
+              reject_all_btn: 'Zaakceptuj wymagane',
+              close_btn_label: 'Zamknij',
+              cookie_table_headers: [
+                  {col1: 'Nazwa'},
+                  {col2: 'Domena'},
+                  {col3: 'Wygaśnięcie'},
+                  {col4: 'Opis'}
+              ],
+              blocks: [
+                  {
+                      title: 'Wykorzystanie plików cookie',
+                      description: 'Wykorzystujemy pliki cookie, w celu zapewnienia podstawowych funkcjonalności tej strony, oraz w celu poprawy doświadczeń użytkownika.'
+                  }, {
+                      title: 'Wymagane pliki cookie',
+                      description: 'Te pliki cookie(s) są niezbędne do funkcjonowania strony. Bez tych plików cookies, strona nie będzie działać prawidłowo',
+                      toggle: {
+                          value: 'necessary',
+                          enabled: true,
+                          readonly: true
+                      },
+                      cookie_table: [
+                        {
+                            col1: '^_wgdotnet',
+                            col2: 'wgdotnet.pl',
+                            col3: '90 dni',
+                            col4: 'cookie(s) "techniczne"',
+                            is_regex: true
+                        }
+                    ]
+                  },
+                  {
+                      title: 'Więcej informacji',
+                      description: 'W celu uzyskania odpowiedzi na tematy związane z naszymi politykami ochrony danych osobowych oraz plików cookie, prosimy o: <br/><ul><li>- Zapoznanie się z <a href="/privacy" class="cc-link">polityką ochrony danych osobowych</a></li><li>- Zapoznanie się z <a href="/cookies" class="cc-link">polityką cookies</a></li><li>- <a class="cc-link" href="mailto:leaders@wgdotnet.pl">Kontakt z nami</a></li></ul>',
+                  }
+              ]
+          }
       }
+    }
   })
   ],
   load: {
